ee7a8e11bd29816b635d2a9a3bc2a127a3af5dfb
Kastytis Chainauskas <kastytis.ch@gmail.com>
2018-02-12 11:15:29 +0200

    TLIST-466: sms sending prototype

--- src/main/kotlin/lt/tlistas/loginn/backend/plugin/aws/sns/MessageSender.kt
+++ src/main/kotlin/lt/tlistas/loginn/backend/plugin/aws/sns/MessageSender.kt
@@ -1,5 +1,20 @@
 package lt.tlistas.loginn.backend.plugin.aws.sns
 
-class MessageSender {
+import com.amazonaws.services.sns.AmazonSNSClientBuilder
+import com.amazonaws.services.sns.model.PublishRequest
 
-}
+
+
+    fun main(args: Array<String>) {
+
+      send("","")
+    }
+
+    fun send(message:String, mobileNumber: String) {
+        val snsClient = AmazonSNSClientBuilder.standard().withRegion("eu-west-1").build()
+
+       snsClient.publish(PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(mobileNumber))
+    }
+
