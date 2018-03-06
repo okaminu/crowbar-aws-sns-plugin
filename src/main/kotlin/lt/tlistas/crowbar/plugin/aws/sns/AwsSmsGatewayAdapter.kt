4fc90779d1b309c6b730ff798d6e581cb7942a23
Kastytis Chainauskas <kastytis.ch@gmail.com>
2018-03-06 15:52:18 +0200

    TLIST-466: Does not rethrow exceptions

--- src/main/kotlin/lt/tlistas/crowbar/plugin/aws/sns/AwsSmsGatewayAdapter.kt
+++ src/main/kotlin/lt/tlistas/crowbar/plugin/aws/sns/AwsSmsGatewayAdapter.kt
@@ -1,23 +1,13 @@
 package lt.tlistas.crowbar.plugin.aws.sns
 
-import com.amazonaws.services.sns.model.InternalErrorException
-import com.amazonaws.services.sns.model.InvalidParameterException
 import com.amazonaws.services.sns.model.PublishRequest
 import lt.tlistas.crowbar.api.ConfirmationMessageGateway
-import lt.tlistas.crowbar.api.exception.ConfirmationMessageGatewayException
-import lt.tlistas.crowbar.api.exception.InvalidAddressException
 
 class AwsSmsGatewayAdapter(private val snsClientBuilder: SnsClientBuilder) : ConfirmationMessageGateway {
 
     override fun send(message: String, address: String) {
-        try {
-            snsClientBuilder.build().publish(PublishRequest()
-                    .withMessage(message)
-                    .withPhoneNumber(address))
-        } catch (e: InternalErrorException) {
-            throw ConfirmationMessageGatewayException("Api exception ${e.message}")
-        } catch (e: InvalidParameterException) {
-            throw InvalidAddressException("Mobile number $address is not valid")
-        }
+        snsClientBuilder.build().publish(PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(address))
     }
 }
--- src/test/kotlin/lt/tlistas/crowbar/test/unit/AwsSmsGatewayAdapterTest.kt
+++ src/test/kotlin/lt/tlistas/crowbar/test/unit/AwsSmsGatewayAdapterTest.kt
@@ -1,70 +1,25 @@
 package lt.tlistas.crowbar.test.unit
 
 import com.amazonaws.services.sns.AmazonSNS
-import com.amazonaws.services.sns.model.InternalErrorException
-import com.amazonaws.services.sns.model.InvalidParameterException
-import com.nhaarman.mockito_kotlin.*
-import lt.tlistas.crowbar.api.exception.ConfirmationMessageGatewayException
-import lt.tlistas.crowbar.api.exception.InvalidAddressException
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import lt.tlistas.crowbar.plugin.aws.sns.AwsSmsGatewayAdapter
 import lt.tlistas.crowbar.plugin.aws.sns.SnsClientBuilder
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.ExpectedException
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
 class AwsSmsGatewayAdapterTest {
-
-    @Mock
-    private lateinit var snsClientBuilderMock: SnsClientBuilder
-
-    private lateinit var awsSmsGatewayAdapter: AwsSmsGatewayAdapter
-
-    @Rule
-    @JvmField
-    val expectedException = ExpectedException.none()!!
-
-    @Before
-    fun setUp() {
-        awsSmsGatewayAdapter = AwsSmsGatewayAdapter(snsClientBuilderMock)
-    }
 
     @Test
     fun `Sends SMS to a mobile number`() {
-        val amazonSNS = mock<AmazonSNS>()
-        doReturn(amazonSNS).`when`(snsClientBuilderMock).build()
+        val snsClientBuilderMock = mock<SnsClientBuilder>()
+        val amazonSnsMock = mock<AmazonSNS>()
+        doReturn(amazonSnsMock).`when`(snsClientBuilderMock).build()
 
-        awsSmsGatewayAdapter.send(MESSAGE, MOBILE_NUMBER)
+        AwsSmsGatewayAdapter(snsClientBuilderMock).send("someMessage", "+37012345678")
 
-        verify(amazonSNS).publish(any())
-    }
-
-    @Test
-    fun `Throws ConfirmationMessageGatewayException on Api exception `() {
-        expectedException.expect(ConfirmationMessageGatewayException::class.java)
-        doAnswer {
-            throw mock<InternalErrorException>()
-        }.`when`(snsClientBuilderMock).build()
-
-        awsSmsGatewayAdapter.send(MESSAGE, MOBILE_NUMBER)
-    }
-
-    @Test
-    fun `Throws InvalidAddressException`() {
-        expectedException.expect(InvalidAddressException::class.java)
-        doAnswer {
-            throw mock<InvalidParameterException>()
-        }.`when`(snsClientBuilderMock).build()
-
-        awsSmsGatewayAdapter.send(MESSAGE, MOBILE_NUMBER)
-    }
-
-    companion object {
-        private const val MESSAGE = "someMessage"
-        private const val MOBILE_NUMBER = "+37012345678"
+        verify(snsClientBuilderMock).build()
+        verify(amazonSnsMock).publish(any())
     }
 }
